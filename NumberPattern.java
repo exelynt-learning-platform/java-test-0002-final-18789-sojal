13c94b81058340fdec0739826902d830b8b03351
sojal bhosale <sojalbhosale24@gmail.com>
2026-03-14 14:18:57 +0530

    refactored

--- NumberPattern.java
+++ NumberPattern.java
@@ -1,15 +1,17 @@
+
 public class NumberPattern {
     public static void main(String[] args) {
         int rows = 5;
+
         for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= rows - i; j++) {
+            for (int space = 1; space <= rows - i; space++) {
                 System.out.print("  ");
             }
-            for (int j = 1; j <= i; j++) {
-                System.out.print(j + " ");
+            for (int num = 1; num <= i; num++) {
+                System.out.print(num + " ");
             }
-            for (int j = i - 1; j >= 1; j--) {
-                System.out.print(j + " ");
+            for (int num = i - 1; num >= 1; num--) {
+                System.out.print(num + " ");
             }
             System.out.println();
         }
